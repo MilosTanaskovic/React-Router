52eef5cf8149209b5475be2f837cdd0bc089233d
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-09-07 18:43:00 +0200

    set up and creating React Router...

---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Link}  from 'react-router-dom';
+
+function Nav() {
+
+    const navStyle = {
+        color : 'white'
+    };
+  return (
+    <nav className="nav-links">
+        <h3>Logo</h3>
+        <ul>
+            <Link style={navStyle} to='/about'>
+                <li>About</li>   
+            </Link>
+            <Link style={navStyle} to='/shop'>
+                <li>Shop</li>   
+            </Link>
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
